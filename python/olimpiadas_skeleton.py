cc7a45eb9a66632007688f32d59dc00fe7f5ddc6
sam.code/ <142764825+FlipperSAM@users.noreply.github.com>
2026-04-11 23:32:01 -0500

    Create olimpiadas_skeleton.py

---
+++ python/olimpiadas_skeleton.py
@@ -0,0 +1,5 @@
+"""
+Módulo: olimpiadas_skeleton.py
+Descripción: Cálculo de velocidad media en Skeleton (Olimpiadas de Invierno).
+Conceptos: Conversión de tiempo, operadores aritméticos y precisión float.
+"""
