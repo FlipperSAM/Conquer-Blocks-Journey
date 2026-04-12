a1d6777095b17c20dd79290cf00adcf8cce63136
sam.code/ <142764825+FlipperSAM@users.noreply.github.com>
2026-04-11 23:32:16 -0500

    Update olimpiadas_skeleton.py

--- python/olimpiadas_skeleton.py
+++ python/olimpiadas_skeleton.py
@@ -3,3 +3,9 @@ Módulo: olimpiadas_skeleton.py
 Descripción: Cálculo de velocidad media en Skeleton (Olimpiadas de Invierno).
 Conceptos: Conversión de tiempo, operadores aritméticos y precisión float.
 """
+def calcular_velocidad_atleta(nombre):
+    print(f"\n--- Datos de {nombre} ---")
+    # 1. Entrada de datos por pantalla
+    minutos = int(input(f"Introduce los minutos de {nombre}: "))
+    segundos = int(input(f"Introduce los segundos de {nombre}: "))
+    centesimas = int(input(f"Introduce las centésimas de {nombre}: "))
