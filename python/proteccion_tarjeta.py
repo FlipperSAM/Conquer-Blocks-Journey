4bdba845cb17c6c3f178885023109d96c03f4d32
sam.code/ <142764825+FlipperSAM@users.noreply.github.com>
2026-04-12 21:18:44 -0500

    Create proteccion_tarjeta.py

---
+++ python/proteccion_tarjeta.py
@@ -0,0 +1,5 @@
+"""
+Módulo: proteccion_tarjeta.py
+Descripción: Script para enmascarar números de tarjeta de crédito (Data Masking).
+Conceptos: Slicing de strings, longitud dinámica y seguridad de datos.
+"""
